b6de01448b045fdd12b19a5f73de84b00c9d43c2
amal625 <abda2023@mymail.pomona.edu>
2025-05-07 21:10:16 -0700

    created files
    Co-authored-by: lilygalvan <lilygalvan@users.noreply.github.com>
    Co-authored-by: knair9 <knair9@users.noreply.github.com>

---
+++ src/thePlug/ArrayList.java
@@ -0,0 +1,10 @@
+package thePlug;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ArrayList {
+
+
+    
+}
---
+++ src/thePlug/BSTInterface.java
@@ -0,0 +1,73 @@
+package thePlug;
+
+public interface BSTInterface {
+    private String
+
+    /**
+     * Inserts a new node with the given data into the BST.
+     *
+     * @param data the data to be inserted
+     */
+    void insert(T data){
+
+    }
+
+    /**
+     * Deletes a node with the given data from the BST.
+     *
+     * @param data the data to be deleted
+     */
+    void delete(T data){
+
+    }
+
+    /**
+     * Searches for a node with the given data in the BST.
+     *
+     * @param data the data to search for
+     * @return true if the data is found, false otherwise
+     */
+    boolean search(T data){
+
+    }
+
+    /**
+     * Returns the minimum value in the BST.
+     *
+     * @return the minimum value
+     */
+    T findMin(){
+
+    }
+
+    /**
+     * Returns the maximum value in the BST.
+     *
+     * @return the maximum value
+     */
+    T findMax(){
+
+    }
+
+    /**
+     * Performs an inorder traversal of the BST and prints the data.
+     */
+    void inorderTraversal(){
+
+    }
+
+    /**
+     * Performs a preorder traversal of the BST and prints the data.
+     */
+    void preorderTraversal(){
+
+    }
+
+    /**
+     * Performs a postorder traversal of the BST and prints the data.
+     */
+    void postorderTraversal(){
+        
+    }
+    
+}
